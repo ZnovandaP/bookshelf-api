7be34aab272cd70f38acc6a79abf23595d801568
ZnovandaP <znovandaputra@gmail.com>
2024-04-20 09:21:43 +0700

    feat[add handler on routes] add handler addBook on the router '/books' with 'POST' method

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,12 @@
 import * as Hapi from '@hapi/hapi';
+import { addBook } from '../handler';
 
 const routes: Hapi.ServerRoute[] = [
-
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBook,
+  },
 ];
 
 export default routes;
